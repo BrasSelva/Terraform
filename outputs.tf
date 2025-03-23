84c16688b35415eefedc161a556b4c465375b3b1
BrasSelva <147380508+BrasSelva@users.noreply.github.com>
2025-03-23 16:58:20 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,12 @@ output "storage_account_name" {
 output "container_name" {
   value = azurerm_storage_container.static_files.name
 }
+
+output "public_ip_address" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
+
+output "flask_app_url" {
+  description = "URL de l'application Flask"
+  value       = "http://${azurerm_public_ip.public_ip.ip_address}:${var.flask_port}/"
+}
