cbb5288c9957e07467e9eb12037460dae02441a6
BrasSelva <147380508+BrasSelva@users.noreply.github.com>
2025-03-23 16:59:14 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,18 +2,40 @@ variable "resource_group_name" {
   type    = string
   default = "rg-flask-tf"
 }
+variable "container_name" {
+  description = "Nom du conteneur de stockage pour les fichiers statiques"
+  type        = string
+  default     = "staticfiles"
+}
 
 variable "location" {
-  type    = string
-  default = "westeurope"
+  default = "East US"
+}
+
+variable "vm_admin_username" {
+  default = "azureuser"
+}
+
+variable "vm_admin_password" {
+  description = "Password for the VM"
+  sensitive   = true
+}
+
+variable "vm_name" {
+  default = "flask-vm"
 }
 
 variable "storage_account_prefix" {
-  type    = string
-  default = "flaskstor"
+  default = "flaskstorage"
 }
 
-variable "container_name" {
-  type    = string
-  default = "staticfiles"
+variable "flask_port" {
+  description = "Port sur lequel l'application Flask écoute"
+  type        = number
+  default     = 5000
+}
+
+variable "connection_string" {
+  description = "Azure Storage Connection String"
+  sensitive = true
 }
